1c5c5e5abcbda6fd44ded6c932597ffdfe91f4c4
mcruger <mcruger@mcruger-VirtualBox.(none)>
2015-03-21 18:08:52 -0500

    jinja2 template rendering example

--- app/views.py
+++ app/views.py
@@ -5,7 +5,18 @@ from app import app
 @app.route('/index')
 def index():
     user = {'nickname': 'Carl'} #mock user obj
+    posts = [ #mock posts obj
+        {
+            'author': {'nickname': 'Jack'},
+            'body': 'Good surf today brah!'
+        },
+        {
+            'author': {'nickname': 'Sally'},
+            'body': 'Guardians of the Galaxy. So good.'
+        }
+    ]
     return render_template('index.html',
                            title='Home',
-                           user=user)
+                           user=user,
+                           posts=posts)
 
